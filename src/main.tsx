58a0e67251af58ecd486161a295729d53c00b842
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-06-12 06:36:10 +0900

    add react-liff
    liff-mockに対応してないのでマージしないほうが良さそう

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { LiffProvider } from 'react-liff';
+
+const isMockMode = import.meta.env.VITE_LIFF_MOCK_MODE === 'true';
+console.debug({ isMockMode });
+const liffId = import.meta.env.VITE_LIFF_ID;
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <LiffProvider liffId={liffId} stubEnabled={isMockMode}>
+        <App />
+      </LiffProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
