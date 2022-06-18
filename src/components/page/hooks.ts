a70af81b370e3e598cab1419eaa2198da3d5529f
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-06-19 04:30:08 +0900

    refactor

--- src/components/page/hooks.ts
+++ src/components/page/hooks.ts
@@ -7,45 +7,6 @@ export const isMockMode = import.meta.env.VITE_LIFF_MOCK_MODE === 'true';
 const liffId = import.meta.env.VITE_LIFF_ID;
 const redirectUri = import.meta.env.VITE_LIFF_REDIRECT_URI;
 
-interface UseProfilePropsInterface {
-  liffId: string;
-}
-
-type ReturnPromiseType<T> = T extends (...args: any[]) => Promise<infer R>
-  ? R
-  : any;
-
-type Profile = ReturnPromiseType<typeof liff.getProfile>;
-
-export const useProfile = async (
-  props: UseProfilePropsInterface
-  // TODO: any を駆逐する
-  // /node_modules/@liff/get-profile/lib/index.d.ts にあるけど、exportされてない？
-): Promise<any> => {
-  // ): Promise<Profile> => {
-  try {
-    await init(props.liffId);
-    return await liff.getProfile();
-  } catch (e) {
-    throw e;
-  }
-};
-
-export const useIdToken = async (props: UseProfilePropsInterface) => {
-  try {
-    await init(props.liffId);
-    return await liff.getIDToken();
-  } catch (e) {
-    throw e;
-  }
-};
-
-const init = async (liffId: string) => {
-  return liff.init({
-    liffId,
-  });
-};
-
 export const fetchMember = async (memberId: string): Promise<Member> => {
   const response = await fetch(`http://localhost:8000/api/members/${memberId}`);
   return await response.json();
