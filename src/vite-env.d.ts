2bd2ed5005a44beb8584d727a142f79229c08db5
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-07-15 00:29:37 +0900

    upd

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_LIFF_ID: string;
+  readonly VITE_LIFF_MOCK_MODE: 'true' | 'false';
+  readonly VITE_LIFF_REDIRECT_URI: string;
+  readonly VITE_LIFF_API_ENDPOINT: string;
+  readonly VITE_LIFF_CODE_TYPE: 'barcode' | 'qrcode';
 }
 
 interface ImportMeta {
