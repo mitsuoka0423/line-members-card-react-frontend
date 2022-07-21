5223354084d40b792914bb04acb0a25b21b5c0d6
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-07-21 19:48:11 +0900

    upd

---
+++ src/types/axios.d.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare module 'axios' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export type AxiosResponse<T = any> = Promise<T>;
+}
